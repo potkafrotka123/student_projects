cb21b4ada417fd0ffedcc9b54d30ba3486fb1373
potkafrotka123 <wkoziolek_221i3@e.zseeim.edu.pl>
2024-03-05 09:30:23 +0000

    studentmachine send

--- cpp/cwiczenie2/main.cpp
+++ cpp/cwiczenie2/main.cpp
@@ -3,23 +3,22 @@
 using namespace std;
 
 int main(){
-	int liczby[9];
+	int liczba = 0;
 	int suma = 0;
+	
+	cout<<"Podaj 10 liczb calkowitych"<<endl;
 	
 	for(int i=1; i<11; i++){
 		cout<<"Podaj liczbe "<<i<<": ";
-		cin>>liczby[i-1];
-		}
-	
-	for(int i=0; i<10; i++){
-		suma=suma+liczby[i];
+		cin>>liczba;
+		suma = suma+liczba;
 		}
 	
 	if(suma%2 == 0){
 		cout<<"Wynik jest parzysty"<<endl;
 		}
 	else{
-		cout<<"Wynik jest nieparzysty"<<endl;
+		cout<<"Wynik jest nie parzysty"<<endl;
 		}
 	
 	return 0;
