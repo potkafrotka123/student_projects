0248412d72cc855aa67350ce93fd9262b35cc35c
potkafrotka123 <wkoziolek_221i3@e.zseeim.edu.pl>
2024-03-19 09:24:41 +0000

    wyslanie pracy

--- python/cwiczenie1/main.py
+++ python/cwiczenie1/main.py
@@ -2,14 +2,21 @@ from rich.console import Console
 
 console = Console()
 
+licznik = 0
+
 #a = 0
 
 #a = int(input("Podaj temperature: "))
 
 #if a>=-5 and a<10:
-	print("Zima")
+	#print("Zima")
 #if a>=0 and a<10:
-	print("Przedwiośnie")
+	#print("Przedwiośnie")
 #if a>10 and a<15:
-	print("Wiosna")
+	#print("Wiosna")
 
+while licznik < 10:
+	z = int(input("Podaj wartość z: "))
+	if z==5:
+		break
+	licznik+=1
