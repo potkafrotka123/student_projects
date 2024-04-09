2d9a7fb83a05e758aea19efcdcb7910934fcdd09
potkafrotka123 <wkoziolek_221i3@e.zseeim.edu.pl>
2024-04-09 08:01:58 +0000

    studentmachine send

---
+++ python/cwiczenie2/main.py
@@ -0,0 +1,14 @@
+#Deklarowanie zmiennych
+tablica = [0, 0, 0]
+suma = 0
+
+#Wczytywanie zmiennych i sumowanie
+for i in range(0, 3):
+	tablica[i] = int(input('Podaj wartosc '+str(i+1)+': '))
+	suma += tablica[i]
+
+#Wypisanie sumy
+if suma == 0:
+	print('Tablica jest pusta')
+else:
+	print('Suma wartosci wynosi: '+str(suma))
